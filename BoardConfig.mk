LineageOS/android_device_lge_ls990

ls990: rebrand CM to Lineage
Change-Id: I1d06d32e98490a71658f774793750e38626e6ee5

BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +26,7 @@ BOARD_CUSTOM_BT_CONFIG := device/lge/ls990/bluetooth/vnd_g3.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_ls990_defconfig
+TARGET_KERNEL_CONFIG := lineage_ls990_defconfig
 TARGET_REQUIRES_BUMP := true
 
 # Partitions

CypherOS/device_lge_ls990

Change-Id: I1f01bb3db1eb17fcb8f85b6c98a2c254315277c1

BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +26,7 @@ BOARD_CUSTOM_BT_CONFIG := device/lge/ls990/bluetooth/vnd_g3.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_ls990_defconfig
+TARGET_KERNEL_CONFIG := lineageos_ls990_defconfig
 TARGET_REQUIRES_BUMP := true
 
 # Partitions